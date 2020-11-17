3a2c0e5a7844c59955dcdc7c0e15564fc17e9923
Anish <anish@codeanish.com>
2020-11-17 07:16:27 +0000

    Project Init

---
+++ src/components/layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../styles/global.scss"
+
+const Layout = ({children}) =>{
+
+    return(
+        <div>
+            {children}
+        </div>
+    )
+}
+
+export default Layout;
