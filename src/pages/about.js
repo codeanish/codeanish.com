92a16f366f45877a46c6517dcfc88d9f435861c4
Anish <anish@codeanish.com>
2020-11-22 21:08:20 +0000

    About page changes.

--- src/pages/about.js
+++ src/pages/about.js
@@ -26,7 +26,7 @@ const About = () => {
 
         fileName: file(relativePath: {eq: "assets/AnishProfilePictureCircular.png"}){
             childImageSharp{
-                fixed(width: 250){
+                fixed(width: 250, height: 250){
                     ...GatsbyImageSharpFixed
                 }
             }
@@ -43,8 +43,9 @@ const About = () => {
                 <div className={styles.name}>Anish Patel</div>
                 <p>Hi I'm Anish. I'm a software engineer and architect based out of London, UK.</p>
                 <p>I've been building great software for my clients for over 12 years and I've had the opportunity to work at some amazing companies.</p>
-                <p>Currently I work at RMS, helping RMS' clients improve their business processes through technology. I lead a team of developers who help to integrate RMS' clients own applications with RMS' platform.</p>
-                <p>I've always worked very closely with the business. It is my opinion that by working closely with the users, you're able to better understand their needs and build a better overall solution.</p>      
+                <p>Currently I work at RMS, helping RMS' clients improve their business processes through technology. I lead a team of developers who help to integrate our clients own applications with RMS' platform.</p>
+                <p>I've always worked very closely with the business. It is my opinion that by working closely with the users, you're able to better understand their needs and build a better overall solution.</p>
+                <p>I'm not currently looking for any new opportunities, but I'm always happy to chat if you have any questions that you think I could help with. Reach out to me on Twitter <a href="https://twitter.com/codeanish" target="_blank">@codeanish</a>.</p>
             </div>
         </Layout>
     );
