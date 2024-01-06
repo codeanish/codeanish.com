7e1e3f072454154b3901d14bd32a1d70fe5fde87
codeanish <anish@codeanish.com>
2024-01-06 00:09:11 -0800

    Force Static pages, removed feed

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,10 @@ import remarkGfm from 'remark-gfm'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  output: 'export',
+  images: {
+    unoptimized: true
+  }
 }
 
 const withMDX = nextMDX({
